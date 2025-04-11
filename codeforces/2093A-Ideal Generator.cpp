3fad21bfe74b649985ff47bdb87a7a89507826a4
Fardin Chowdhury <cfardin51@gmail.com>
2025-04-11 22:20:55 +0600

    Create 2093A-Ideal Generator.cpp
    Codeforces : 2093A-Ideal Generator 
    Problem link : https://codeforces.com/contest/2093/problem/A

---
+++ codeforces/2093A-Ideal Generator.cpp
@@ -0,0 +1,17 @@
+// 2093A-Ideal Generator 
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int k;
+        cin >> k;
+        if(k%2!=0) cout << "yes" << endl;
+        else cout << "no" << endl;
+    }
+
+    return 0;
+}
