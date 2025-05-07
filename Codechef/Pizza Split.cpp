2d0d8e8206a6deb7fb285c39b6d6702f0d1ceb63
Fardin Chowdhury <cfardin51@gmail.com>
2025-05-07 22:53:57 +0600

    Create Pizza Split.cpp
    Codechef : Pizza Split
    Problem link : https://www.codechef.com/problems/PZSPLIT

---
+++ Codechef/Pizza Split.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    if(n%2==0) cout << 1 << endl;
+    else cout << 2 << endl;
+    
+    return 0;
+}
+    
