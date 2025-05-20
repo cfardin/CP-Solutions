4fc7425b3dbab610c0571f068ff3e18c4a053095
Fardin Chowdhury <cfardin51@gmail.com>
2025-05-20 23:25:14 +0600

    Create Codechef_Chess Win.cpp

---
+++ Codechef/Codechef_Chess Win.cpp
@@ -0,0 +1,16 @@
+// Codechef problem Chess Win. 
+// Problem link : https://www.codechef.com/problems/CHESSWIN
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    int a, b;
+    cin a >> b;
+    cout << abs(b - a) + 1 << "\n";
+    return 0;
+}
+
