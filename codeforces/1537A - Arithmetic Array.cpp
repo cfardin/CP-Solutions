ad04635ef1a03e014a87d2eebfcb0689b5816908
Fardin Chowdhury <cfardin51@gmail.com>
2025-05-20 23:14:01 +0600

    Create 1537A - Arithmetic Array.cpp

---
+++ codeforces/1537A - Arithmetic Array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, sum(0);
+        cin >> n;
+        for(int i=0; i<n; i++){
+            int a;
+            cin >> a;
+            sum += a;
+        }
+        if(sum < n) cout << 1 << "\n";
+        else cout << sum - n << endl;
+
+    }
+    return 0;
+}
+
