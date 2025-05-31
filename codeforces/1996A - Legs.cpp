89b90df958f479630ef09b51acd5fc370c74db0f
Fardin Chowdhury <cfardin51@gmail.com>
2025-05-31 09:59:33 +0600

    Create 1996A - Legs.cpp

---
+++ codeforces/1996A - Legs.cpp
@@ -0,0 +1,21 @@
+//Codeforces : 1996A - Legs
+//Pkroblem link : https://codeforces.com/problemset/problem/1996/A
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    cout << (n + 2)/4 << endl;
+}
+
+int main(){
+    ios :: sync_with_stdio(false);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
