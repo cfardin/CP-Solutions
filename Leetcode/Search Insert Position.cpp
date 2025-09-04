b504dd54a4d523e1a150b5080ce98793cc0d2d37
Fardin Chowdhury <cfardin51@gmail.com>
2025-09-04 11:38:41 +0600

    Create Search Insert Position.cpp

---
+++ Leetcode/Search Insert Position.cpp
@@ -0,0 +1,23 @@
+// Search Insert Position Leetcode
+// Problem link : https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n-1;
+        int ans = n;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] >= target){
+                ans = min(ans, mid);
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
